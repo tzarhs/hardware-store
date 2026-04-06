e8560b1652839780bf8d223160959fbd6ec450a3
tzarhs <jtzaris@gmail.com>
2026-04-06 13:10:45 +0300

    Added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
@@ -30,6 +31,7 @@ function App() {
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms-of-service" element={<TermsOfService />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
